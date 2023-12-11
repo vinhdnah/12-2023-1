1e567199b34c3e97616125c1dac0b414f9eec403
vinhdnah <141840561+vinhdnah@users.noreply.github.com>
2023-12-11 21:11:57 +0700

    V3
    V3

---
+++ v3.py
@@ -0,0 +1,15 @@
+import random
+total = random.randint(3,18)
+input("Nhập Số Phiên: ")
+print("Thường")
+if total <=10:
+	print("X" + str(total))
+elif total >=11:
+	print("T" + str(total))
+input("nhập số phiên:")
+print("md5")
+md5kq = random.randint(3,18)
+if md5kq <=10:
+	print("X" + str(md5kq))
+elif md5kq >=11:
+	print("T" + str(md5kq))
